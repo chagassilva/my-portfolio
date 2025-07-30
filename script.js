e4284339953069807c7beef54f761d3a3595b608
chagassilva <chagassilvati@gmail.com>
2025-07-29 21:55:48 -0300

    Add animations na sections

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+ function showSection(id) {
+  const sections = document.querySelectorAll(".section");
+  sections.forEach((sec) => {
+    if (sec.id === id) {
+      sec.classList.add("active");
+    } else {
+      sec.classList.remove("active");
+    }
+  });
+}
 
 
+const toggleButton = document.getElementById("toggleButton");
 
-particlesJS.load('particles-js', 'particles-config.json', function() {
-      console.log('Particles carregado com sucesso!');
-    });
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    toggleButton.src = "assets/night-mode.png";
+    toggleButton.style.width = "25px"; 
+    toggleButton.style.height = "25px";
+     // ícone da lua
+  } else {
+    toggleButton.src = "assets/light.png"; // ícone do sol
+  }
+});
